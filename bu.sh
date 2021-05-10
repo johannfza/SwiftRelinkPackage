9706b1d50b8475709566264157bdd7699b2f2a11
Johann Fong <johannfza@gmail.com>
2021-05-10 18:26:15 +0800

    chore: clean up with bu file

--- bu.sh
+++ bu.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 #######################################
-# Exit bash script with error message
+# Quit bash script with exit message
 # Arguments:
 #   message
 #######################################
 function err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] ERROR: $*" >&2
-    echo "relink.sh help for helper"
     exit 1
 }
 
@@ -22,9 +21,11 @@ then
   err "usage - bash relink.sh <path-to-xcodeproj-file> <branch> <domain-branch-optional>"
 fi
 
-# create local variable 
+# [ "$#" -eq 3 ] || leave "3 argument required, $# provided"
+
 path=$1
 branch=$2
+
 if [ ! $3 ]
 then 
   domain_branch="master"
@@ -35,6 +36,14 @@ fi
 echo "path: $1"
 echo "branch: $2"
 echo "domain_branch: $domain_branch"
+
+# function downloadRelink() {
+#     echo "Downloading relink"
+#     git clone https://github.com/johannfza/SwiftRelinkPackage.git
+#     cd SwiftRelinkPackage
+#     echo "relink installed"
+#     cd ..
+# }
 
 #######################################
 # Check if path is a valid XcodeProj
@@ -57,17 +66,33 @@ function check_path_is_valid_xcodeproj() {
   echo "$path is valid"
 }
 
+#######################################
+# Check if path is a valid XcodeProj
+# Globals:
+#   PATH
+# Arguments:
+#   Path to XcodeProj
+#######################################
+
+
 check_path_is_valid_xcodeproj $path
 
 if ! command -v swift run relink &> /dev/null
 then
     err "relink could not be found" "make sure relink is visible in this directory"
+
+    # echo "alternatively do you want to download relink?"
+    # select yn in "Yes" "No"; do
+    #     case $yn in
+    #         Yes ) downloadRelink; break;;
+    #         No ) echo "relink NOT installed"; exit;;
+    #     esac
+    # done
+    # downloadRelink
     exit 1
 fi 
 
 echo "relink installed"
 echo "Starting relink"
-
-# TODO(): 
-
+# Start herer 
 exit 1
